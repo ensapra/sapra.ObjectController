1a4182be9f6fd5e312f4ba98d44282b45018eb86
Pau Solsona <pausolsonaboada@gmail.com>
2022-06-06 11:48:23 +0200

    Update AbstractRoutine.cs

--- Runtime/AbstractRoutine.cs
+++ Runtime/AbstractRoutine.cs
@@ -15,7 +15,7 @@ namespace sapra.ObjectController
         internal bool wantsAwakened{get{return wantsAwake;}}
         internal bool awakened{get{return isAwake;}}
 
-        [SerializeField] protected AbstractCObject controller;
+        [SerializeField] [HideInInspector] protected AbstractCObject controller;
         internal void Awake(AbstractCObject controller)
         {
             this.controller = controller;
